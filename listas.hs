5d04ce9b8e749788127a3852939e90d04b6169d4
Paula Vaz <pcosv@cin.ufpe.br>
2019-08-21 14:25:09 -0300

    Listas

---
+++ listas.hs
@@ -0,0 +1,20 @@
+
+double :: [Int] -> [Int]
+double [] = []
+double (x:xs) = x * 2 : double xs
+
+
+member :: [Int] -> Int -> Bool
+member [] n = False
+member (x:xs) n
+ | x == n = True
+ | otherwise = member xs n
+
+digito :: Char -> Bool
+digito ch = ('0' <= ch) && (ch <= '9')
+
+digits :: String -> String
+digits [] = []
+digits (x:xs)
+ | digito x = x : digits xs
+ | otherwise = digits xs 
