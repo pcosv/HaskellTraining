67a7b6a0457856b7957da83d7b6585b33df289f2
Paula Vaz <pcosv@cin.ufpe.br>
2019-09-23 23:02:43 -0300

    first exercises list: 7 done

--- Lista01.hs
+++ Lista01.hs
@@ -67,3 +67,30 @@ somaCem :: Int -> Int
 somaCem n = n + 100
 
 -- Questão 7
+type Codigo = Int
+
+data Voto
+  = Presidente Codigo
+  | Senador Codigo
+  | Deputado Codigo
+  | Branco deriving (Show, Eq)
+
+type Urna = [Voto]
+
+type Apuracao = [(Voto, Int)]
+
+urna = [(Presidente 5000), (Presidente 5001), (Senador 100), (Senador 101),
+  (Deputado 50000), (Deputado 50001), (Deputado 50001), (Deputado 50001)]
+
+-- (a)
+totalVotos :: Urna -> Voto -> Int
+totalVotos [] v = 0
+totalVotos (x:xs) v =
+  if x == v
+  then 1 + totalVotos xs v
+  else totalVotos xs v
+
+-- (b)
+apurar :: Urna -> Apuracao
+apurar [] = []
+apurar (u:us) = (u, totalVotos (u:us) u) : apurar (filter (\x -> x /= u) us)
